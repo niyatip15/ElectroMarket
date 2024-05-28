964769fa12dd2a179f98dfb482c32505ce0e774c
niyatip15 <niyupathak1503@gmail.com>
2024-05-28 20:14:02 +0530

    Implemented React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
 
@@ -13,6 +15,8 @@ function App() {
           {/* Use Routes component to wrap Route components */}
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+
           </Routes>
         </Container>
       </main>
