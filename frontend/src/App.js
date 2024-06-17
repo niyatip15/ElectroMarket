83ce48e7c1bf74ec645849f7609a24a6aa1bb6d8
Niyati Pathak <niyupathak1503@gmail.com>
2024-06-17 10:27:13 +0530

    Qty select and cart.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
@@ -16,6 +17,8 @@ function App() {
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
             <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart/:id?' element={<CartScreen />} />
+
 
           </Routes>
         </Container>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import axios from 'axios';
 import Loader from '../components/Loader';
 import Message from '../components/Message'; 
 
-function ProductScreen() {
+function ProductScreen({history}) {
   const { id } = useParams(); 
+  const navigate = useNavigate();
+  const [qty,setQty] = useState(1)
   const [product, setProduct] = useState(null); 
   const [loading, setLoading] = useState(true); 
   const [error, setError] = useState(null);
@@ -36,6 +38,9 @@ function ProductScreen() {
     return <Message variant="danger">{error}</Message>; 
   }
 
+  const addToCartHandler = () => {
+    navigate(`/cart/${id}?qty=${qty}`);
+  };
   return (
     <div>
       <Link to='/' className='btn btn-light my-3'>Go Back</Link>
@@ -83,9 +88,30 @@ function ProductScreen() {
                   </Col>
                 </Row>
               </ListGroup.Item>
+              {product.countInStock > 0 && (
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Qty</Col>
+                    <Col xs='auto' className='my-1'>
+                      <Form.Control as="select" value={qty}
+                      onChange={(e) => setQty(e.target.value)}>
+                        {
+                          [...Array(product.countInStock).keys()].map((x) =>(
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))
+                        }
+                      </Form.Control>
+
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
 
               <ListGroup.Item>
                 <Button
+                  onClick={addToCartHandler}
                   className='btn-block'
                   disabled={product.countInStock === 0}
                   type='button'
