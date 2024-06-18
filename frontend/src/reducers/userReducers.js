90231add0f2aaa06c66ac730908c7bee746208c6
Niyati Pathak <niyupathak1503@gmail.com>
2024-06-18 16:30:03 +0530

    get user details.

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,5 +1,9 @@
 import { USER_LOGIN_FAIL,USER_LOGIN_REQUEST,USER_LOGIN_SUCCESS,USER_LOGOUT,
-    USER_REGISTER_FAIL,USER_REGISTER_REQUEST,USER_REGISTER_SUCCESS
+    USER_REGISTER_FAIL,USER_REGISTER_REQUEST,USER_REGISTER_SUCCESS,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_RESET,
  } from '../constants/userConstants'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -34,6 +38,26 @@ export const userRegisterReducer = (state = {}, action) => {
 
         case USER_LOGOUT:
             return {}
+
+        default:
+            return state
+    }
+}
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+    switch (action.type) {
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true }
+
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload }
+
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+
+        case USER_DETAILS_RESET:
+            return { user: {} }
+
 
         default:
             return state
