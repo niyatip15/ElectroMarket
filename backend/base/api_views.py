6f4746f6884a07252ffe2d68eca9cd4f23378a3e
Niyati Pathak <niyupathak1503@gmail.com>
2024-06-18 09:28:40 +0530

    login api

--- backend/base/api_views.py
+++ backend/base/api_views.py
@@ -1,18 +1,25 @@
 from rest_framework.views import APIView
+from django.utils.decorators import method_decorator
+from django.middleware.csrf import get_token
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.generics import ListAPIView,RetrieveAPIView
 from rest_framework import permissions,authentication
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from .models import *
 from .serializer import *
 # Create your views here.
 class ProductListAPIView(ListAPIView):
+    permission_classes = [AllowAny]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ProductDetailAPIView(RetrieveAPIView):
+    permission_classes = [AllowAny]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = '_id'
@@ -33,6 +40,7 @@ class RegisterUserAPIView(APIView):
         existing_user = User.objects.filter(email=data['email']).first()
         if existing_user:
             return Response({'message': 'User with this email already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+        
         user = User.objects.create(
             first_name=data['name'],
             username=data['email'],
@@ -40,4 +48,23 @@ class RegisterUserAPIView(APIView):
             password=make_password(data['password'])
         )
         serializer = UserSerializer(user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({
+            **serializer.data,
+        }, status=status.HTTP_201_CREATED)
+@method_decorator(csrf_exempt, name='dispatch')
+class LoginAPIView(APIView):
+        permission_classes = [IsAuthenticated]  
+
+        def post(self, request, format=None):
+            email = request.data.get('email')
+            password = request.data.get('password')
+            if not email or not password:
+                return Response({'message': 'Both email and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                return Response({'message': 'Login successful.'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'Invalid credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('products/<int:_id>/', ProductDetailAPIView.as_view(), name='product-detail'),
     path('users/profile/', GetUserProfileAPIView.as_view(), name='users-profiles'),
     path('user/register/', RegisterUserAPIView.as_view(), name='user-register'),
+    path('user/login/', LoginAPIView.as_view(), name='user-login'),
     
     
     
